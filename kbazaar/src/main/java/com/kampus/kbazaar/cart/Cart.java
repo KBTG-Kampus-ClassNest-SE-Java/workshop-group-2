7d869edc7548819aa1912554e60d68640089ff4e
naruepracht <naruepracht.s@gmail.com>
2024-03-23 14:45:45 +0700

    edit cart and kpoint schema and edit Cart entity

--- kbazaar/src/main/java/com/kampus/kbazaar/cart/Cart.java
+++ kbazaar/src/main/java/com/kampus/kbazaar/cart/Cart.java
@@ -1,16 +1,29 @@
 package com.kampus.kbazaar.cart;
 
 import com.kampus.kbazaar.product.Product;
+import com.kampus.kbazaar.shopper.Shopper;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity(name = "cart")
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Cart {
-    private int userID;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @OneToOne
+    @JoinColumn(name = "shopper_id", referencedColumnName = "id")
+    private Shopper shopper;
+
+    @OneToMany
+    @JoinColumn(name = "product_id", referencedColumnName = "id")
     private Product[] products;
 
-    public Cart(int userID, Product[] products) {
-        this.userID = userID;
-        this.products = products;
-    }
 }
