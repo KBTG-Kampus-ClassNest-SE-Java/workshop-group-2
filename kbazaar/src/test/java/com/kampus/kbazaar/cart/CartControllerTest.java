68ef6729f0080e950787d9693a4aaeac49f2add4
naruepracht <naruepracht.s@gmail.com>
2024-03-23 23:05:58 +0700

    fix CartControllerTest fail

--- kbazaar/src/test/java/com/kampus/kbazaar/cart/CartControllerTest.java
+++ kbazaar/src/test/java/com/kampus/kbazaar/cart/CartControllerTest.java
@@ -10,7 +10,9 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.http.MediaType;
@@ -19,6 +21,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 @ExtendWith(SpringExtension.class)
+@AutoConfigureMockMvc(addFilters = false)
 @WebMvcTest(
         controllers = CartController.class,
         excludeFilters =
@@ -29,12 +32,15 @@ public class CartControllerTest {
 
     @Autowired private MockMvc mockMvc;
 
-    @InjectMocks private CartController cartController;
+//    @InjectMocks private CartController cartController;
+
+    @MockBean
+    private CartService cartService;
 
     @BeforeEach
     public void setup() {
         MockitoAnnotations.openMocks(this);
-        this.mockMvc = MockMvcBuilders.standaloneSetup(cartController).build();
+//        this.mockMvc = MockMvcBuilders.standaloneSetup(cartController).build();
     }
 
     @Test
@@ -42,4 +48,12 @@ public class CartControllerTest {
         mockMvc.perform(get("/api/v1/carts").contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
     }
+
+//    @Test
+//    public void applyCodeIfSuccess_ShouldReturnOk() throws Exception {
+//        String username = "TechNinja";
+//        when(cartService.applyCode(anyString(), any())).thenReturn(new ApplyCodeResponse());
+//        mockMvc.perform(post("/api/v1/carts/{username}/promotions", username).contentType(MediaType.APPLICATION_JSON))
+//                .andExpect(status().isOk());
+//    }
 }
